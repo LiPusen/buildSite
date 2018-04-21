865a2885425bff1a00985c6d8c4f5c3753b37117
LiPusen <283008477@qq.com>
2018-04-21 22:49:52 +0800

    u

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var path = require('path');
-var _ = require('lodash');
 var express = require('express');
+// 服务端压缩插件，以压缩包的形式将文件发送给前端，前端接收后解压运行
 var compression = require('compression');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// 请求参数校验中间件
 var validator = require('./lib/validator.lib');
+// 用户认证中间件
 var session = require('./lib/session.lib');
+// 系统日志中间件
 var logger = require('./lib/logger.lib');
+// 路由映射中间件，包括所有的页面请求和接口请求
 var router = require('./lib/route-map.lib');
+// 全局错误处理中间件
 var errors = require('./core/controllers/errors.controller').error;
 
 var app = express();
