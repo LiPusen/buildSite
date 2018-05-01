c7fd8a11fcdcb4be4af22b5007c0567d8c962ce5
LiPusen <283008477@qq.com>
2018-05-01 21:04:42 +0800

    install

--- core/controllers/install.controller.js
+++ core/controllers/install.controller.js
@@ -10,19 +10,29 @@ var installService = require('../services/install.service');
  * @param {Object} res
  * @param {Function} next
  */
-exports.access = function (req, res, next) {
-  installService.status(function (err, hasInstall) {
-    if (err) {
-      logger[err.type]().error(__filename, err);
-      return res.status(500).end();
-    }
-    
-    if (hasInstall) {
-      next();
-    } else {
-      res.sendFile('notInstalled.html', { root: './public/assets/admin/' });
-    }
-  });
+exports.access = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+	if(req.method == 'OPTIONS') {
+		res.send(200);
+	} else {
+		installService.status(function(err, hasInstall) {
+			if(err) {
+				logger[err.type]().error(__filename, err);
+				return res.status(500).end();
+			}
+
+			if(hasInstall) {
+				next();
+			} else {
+				res.sendFile('index.html', {
+					root: './public/install/'
+				});
+			}
+		});
+	}
 };
 
 /**
@@ -30,19 +40,23 @@ exports.access = function (req, res, next) {
  * @param {Object} req
  * @param {Object} res
  */
-exports.status = function (req, res) {
-  installService.status(function (err, hasInstall) {
-    if (err) {
-      logger[err.type]().error(__filename, err);
-      return res.status(500).end();
-    }
+exports.status = function(req, res) {
+	installService.status(function(err, hasInstall) {
+		if(err) {
+			logger[err.type]().error(__filename, err);
+			return res.status(500).end();
+		}
 
-    if (hasInstall) {
-      res.status(200).json({ hasInstall: true });
-    } else {
-      res.status(200).json({ hasInstall: false });
-    }
-  });
+		if(hasInstall) {
+			res.status(200).json({
+				hasInstall: true
+			});
+		} else {
+			res.status(200).json({
+				hasInstall: false
+			});
+		}
+	});
 };
 
 /**
@@ -50,15 +64,15 @@ exports.status = function (req, res) {
  * @param {Object} req
  * @param {Object} res
  */
-exports.themes = function (req, res) {
-  themes.get(function (err, themes) {
-    if (err) {
-      logger[err.type]().error(__filename, err);
-      return res.status(500).end();
-    }
+exports.themes = function(req, res) {
+	themes.get(function(err, themes) {
+		if(err) {
+			logger[err.type]().error(__filename, err);
+			return res.status(500).end();
+		}
 
-    res.status(200).json(themes);
-  });
+		res.status(200).json(themes);
+	});
 };
 
 /**
@@ -71,96 +85,119 @@ exports.themes = function (req, res) {
  * 				{String} req.body.password
  * @param {Function} res
  */
-exports.testDatabase = function (req, res) {
-  req.checkBody({
-    'host': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'host 不能为空'
-      },
-      matches: {
-        options: [/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$|^localhost$/],
-        errorMessage: 'host 格式不正确'
-      }
-    },
-    'port': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'port 不能为空'
-      },
-      isInt: {
-        options: [{ min: 0, max: 65535 }],
-        errorMessage: 'port 需为整数'
-      }
-    },
-    'db': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'database 不能为空'
-      },
-      isString: { errorMessage: 'database 需为字符串' }
-    },
-    'user': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'user 不能为空'
-      },
-      isString: { errorMessage: 'user 需为字符串' }
-    },
-    'pass': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'password 不能为空'
-      },
-      isString: { errorMessage: 'password 需为字符串' }
-    }
-  });
+exports.testDatabase = function(req, res) {
+	req.checkBody({
+		'host': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'host 不能为空'
+			},
+			matches: {
+				options: [/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$|^localhost$/],
+				errorMessage: 'host 格式不正确'
+			}
+		},
+		'port': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'port 不能为空'
+			},
+			isInt: {
+				options: [{
+					min: 0,
+					max: 65535
+				}],
+				errorMessage: 'port 需为整数'
+			}
+		},
+		'db': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'database 不能为空'
+			},
+			isString: {
+				errorMessage: 'database 需为字符串'
+			}
+		},
+		'user': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'user 不能为空'
+			},
+			isString: {
+				errorMessage: 'user 需为字符串'
+			}
+		},
+		'pass': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'password 不能为空'
+			},
+			isString: {
+				errorMessage: 'password 需为字符串'
+			}
+		}
+	});
 
-  if (req.validationErrors()) {
-    logger.system().error(__filename, '参数验证失败', req.validationErrors());
-    return res.status(400).end();
-  }
+	if(req.validationErrors()) {
+		logger.system().error(__filename, '参数验证失败', req.validationErrors());
+		return res.status(200).json({
+			code: 301,
+			msg: '参数验证失败！',
+			body: req.validationErrors()
+		});
+	}
 
-  var data = {
-    host: req.body.host,
-    port: req.body.port,
-    db: req.body.db,
-    user: req.body.user,
-    pass: req.body.pass
-  };
+	var data = {
+		host: req.body.host,
+		port: req.body.port,
+		db: req.body.db,
+		user: req.body.user,
+		pass: req.body.pass
+	};
 
-  async.series([
-    // 检查安装状态
-    function (callback) {
-      installService.status(function (err, hasInstall) {
-        if (err) return callback(err);
+	async.series([
+		// 检查安装状态
+		function(callback) {
+			installService.status(function(err, hasInstall) {
+				if(err) return callback(err);
 
-        if (hasInstall) {
-          var err = {
-            type: 'system',
-            error: '非法调用，系统已经安装'
-          };
-          return callback(err);
-        }
+				if(hasInstall) {
+					var err = {
+						type: 'system',
+						error: '非法调用，系统已经安装'
+					};
+					return callback(err);
+				}
 
-        callback();
-      });
-    },
-    // 测试数据库连接
-    function (callback) {
-      database.test(data, function (err) {
-        if (err) return callback(err);
+				callback();
+			});
+		},
+		// 测试数据库连接
+		function(callback) {
+			database.test(data, function(err) {
+				if(err) return callback(err);
 
-        callback();
-      });
-    }
-  ], function (err) {
-    if (err) {
-      return res.status(400).end();
-    }
+				callback();
+			});
+		}
+	], function(err) {
+		if(err) {
+			return res.status(200).json({
+				code: 302,
+				msg: '数据库连接验证失败，请核查相关参数！',
+				body: {}
+			});
+		}
 
-    res.status(204).end();
-  });
+		res.status(200).json({
+			code: 200,
+			msg: '数据库连接成功！',
+			body: {
+				info: 'ok'
+			}
+		});
+	});
 };
 
 /**
@@ -178,154 +215,169 @@ exports.testDatabase = function (req, res) {
  * 				{String} req.body.password
  * @param {Function} res
  */
-exports.install = function (req, res) {
-  req.checkBody({
-    'databaseHost': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'databaseHost 不能为空'
-      },
-      matches: {
-        options: [/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$|^localhost$/],
-        errorMessage: 'databaseHost 格式不正确'
-      }
-    },
-    'databasePort': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'databasePort 不能为空'
-      },
-      isInt: {
-        options: [{ min: 0, max: 65535 }],
-        errorMessage: 'databasePort 需为整数'
-      }
-    },
-    'database': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'database 不能为空'
-      },
-      isString: { errorMessage: 'database 需为字符串' }
-    },
-    'databaseUser': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'databaseUser 不能为空'
-      },
-      isString: { errorMessage: 'databaseUser 需为字符串' }
-    },
-    'databasePassword': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'databasePassword 不能为空'
-      },
-      isString: { errorMessage: 'databasePassword 需为字符串' }
-    },
-    'theme': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'theme 不能为空'
-      },
-      isString: { errorMessage: 'theme 需为字符串' }
-    },
-    'title': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'title 不能为空'
-      },
-      isString: { errorMessage: 'title 需为字符串' }
-    },
-    'email': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'email 不能为空'
-      },
-      matches: {
-        options: [/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/],
-        errorMessage: 'email 格式不正确'
-      }
-    },
-    'nickname': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'nickname 不能为空'
-      },
-      isString: { errorMessage: 'nickname 需为字符串' }
-    },
-    'password': {
-      notEmpty: {
-        options: [true],
-        errorMessage: 'password 不能为空'
-      },
-      isLength: {
-        options: [6],
-        errorMessage: 'password 不能小于 6 位'
-      }
-    }
-  });
+exports.install = function(req, res) {
+	req.checkBody({
+		'databaseHost': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'databaseHost 不能为空'
+			},
+			matches: {
+				options: [/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$|^localhost$/],
+				errorMessage: 'databaseHost 格式不正确'
+			}
+		},
+		'databasePort': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'databasePort 不能为空'
+			},
+			isInt: {
+				options: [{
+					min: 0,
+					max: 65535
+				}],
+				errorMessage: 'databasePort 需为整数'
+			}
+		},
+		'database': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'database 不能为空'
+			},
+			isString: {
+				errorMessage: 'database 需为字符串'
+			}
+		},
+		'databaseUser': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'databaseUser 不能为空'
+			},
+			isString: {
+				errorMessage: 'databaseUser 需为字符串'
+			}
+		},
+		'databasePassword': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'databasePassword 不能为空'
+			},
+			isString: {
+				errorMessage: 'databasePassword 需为字符串'
+			}
+		},
+		'theme': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'theme 不能为空'
+			},
+			isString: {
+				errorMessage: 'theme 需为字符串'
+			}
+		},
+		'title': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'title 不能为空'
+			},
+			isString: {
+				errorMessage: 'title 需为字符串'
+			}
+		},
+		'email': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'email 不能为空'
+			},
+			matches: {
+				options: [/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/],
+				errorMessage: 'email 格式不正确'
+			}
+		},
+		'nickname': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'nickname 不能为空'
+			},
+			isString: {
+				errorMessage: 'nickname 需为字符串'
+			}
+		},
+		'password': {
+			notEmpty: {
+				options: [true],
+				errorMessage: 'password 不能为空'
+			},
+			isLength: {
+				options: [6],
+				errorMessage: 'password 不能小于 6 位'
+			}
+		}
+	});
 
-  if (req.validationErrors()) {
-    logger.system().error(__filename, '参数验证失败', req.validationErrors());
-    return res.status(400).end();
-  }
+	if(req.validationErrors()) {
+		logger.system().error(__filename, '参数验证失败', req.validationErrors());
+		return res.status(400).end();
+	}
 
-  var databaseDate = {
-    host: req.body.databaseHost,
-    port: req.body.databasePort,
-    db: req.body.database,
-    user: req.body.databaseUser,
-    pass: req.body.databasePassword
-  };
+	var databaseDate = {
+		host: req.body.databaseHost,
+		port: req.body.databasePort,
+		db: req.body.database,
+		user: req.body.databaseUser,
+		pass: req.body.databasePassword
+	};
 
-  var siteInfodata = {
-    title: req.body.title,
-    theme: req.body.theme
-  };
+	var siteInfodata = {
+		title: req.body.title,
+		theme: req.body.theme
+	};
 
-  var userDate = {
-    email: req.body.email,
-    nickname: req.body.nickname,
-    password: req.body.password
-  };
+	var userDate = {
+		email: req.body.email,
+		nickname: req.body.nickname,
+		password: req.body.password
+	};
 
-  async.auto({
-    status: function (callback) {
-      installService.status(function (err, hasInstall) {
-        if (err) return callback(err);
+	async.auto({
+		status: function(callback) {
+			installService.status(function(err, hasInstall) {
+				if(err) return callback(err);
 
-        if (hasInstall) {
-          var err = {
-            type: 'system',
-            error: '建站系统已经安装'
-          };
-          return callback(err);
-        }
+				if(hasInstall) {
+					var err = {
+						type: 'system',
+						error: '建站系统已经安装'
+					};
+					return callback(err);
+				}
 
-        callback();
-      });
-    },
-    install: ['status', function (callback) {
-      installService.install({
-        databaseDate: databaseDate,
-        siteInfoDate: siteInfodata,
-        userDate: userDate
-      }, function (err, install) {
-        if (err) return callback(err);
+				callback();
+			});
+		},
+		install: ['status', function(callback) {
+			installService.install({
+				databaseDate: databaseDate,
+				siteInfoDate: siteInfodata,
+				userDate: userDate
+			}, function(err, install) {
+				if(err) return callback(err);
 
-        callback(null, install);
-      });
-    }],
-    initTheme: ['install', function (callback) {
-      themes.init(req.app, callback);
-    }]
-  }, function (err) {
-    if (err) {
-      logger[err.type]().error(err);
-      return res.status(500).end();
-    }
+				callback(null, install);
+			});
+		}],
+		initTheme: ['install', function(callback) {
+			themes.init(req.app, callback);
+		}]
+	}, function(err) {
+		if(err) {
+			logger[err.type]().error(err);
+			return res.status(500).end();
+		}
 
-    logger.system().info(__dirname, '系统安装成功！');
+		logger.system().info(__dirname, '系统安装成功！');
 
-    res.status(200).end();
-  });
+		res.status(200).end();
+	});
 };
---
+++ public/install/js/index.js
@@ -0,0 +1,28 @@
+(function(win) {
+	win.install = {}
+	var data = {};
+	install.init = function() {
+		$('#test').click(function(){
+//			if($('#host').val() && $('#port').val() && $('#user').val() && $('#db').val() && $('#pass').val()) {
+				$.ajax({
+					type:"put",
+					url:"/api/install/test-database",
+					async:true,
+					data: {db: $('#db').val(), host: $('#host').val(), pass: $('#pass').val(), user: $('#user').val(), port: $('#port').val()}
+				}).then(function(r){
+					console.log(r)
+				}, function(e) {
+					console.log(e)
+				})
+//			} else {
+//				alert('请完善相关参数的设置再进行数据库连接测试')
+//			}
+		})
+	}
+})(this)
+
+$(function(){
+	install.init();
+})
+
+
